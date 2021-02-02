b6a1a963585325addfa620c8ae2f8ac2f44ad333
Naphat Nithisopa <pection.naphat@gmail.com>
2021-02-03 00:52:06 +0700

    commit dotfiles

---
+++ .dotfiles.oh-my-zsh/custom/hash.zsh
@@ -0,0 +1,10 @@
+hash -d scraper=/home/pection/Backup/Program/scraper-mnfurniture
+hash -d program=/home/pection/Backup/Program
+hash -d mnfurniture=/home/pection/Backup/Program/MNfurniture
+hash -d jupyter_virtual=/home/pection/Backup/Program/jupyter_virtual
+hash -d vimbook=/home/pection/Backup/book/vimbook
+hash -d aibook=/home/pection/Backup/book/
+hash -d gcdl=/home/pection/Downloads/Googlechrome
+hash -d personal=/home/pection/Backup/Personal
+hash -d pandas=/home/pection/Backup/Program/pandas_database
+
